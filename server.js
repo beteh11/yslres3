cb6a0a9a50d7fb895ac5230e9ce9c8fb4d0747cc
beteh11 <beteh_11@hotmail.com>
2024-08-17 08:00:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,19 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
 const escpos = require('escpos');
 
-// Replace with your printer's IP address
-const printerIP = '192.168.1.188';
-const printerPort = 9100;
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 
-io.on('connection', (socket) => {
-  socket.on('printData', (data) => {
-    const device = new escpos.Network(printerIP, printerPort);
+app.post('/print', (req, res) => {
+    const text = req.body.text;
 
-    // Convert data to ESC/POS commands (replace with your logic)
-    device.write(data.text, 'utf-8'); // Example for text printing
+    // Replace with your printer information
+    const device = new escpos.Network('192.168.1.188', 9100); // Replace with your printer IP and port
 
+    // Implement logic to send printing commands to device using the text
+    device.write(text, 'utf-8');
     device.close();
-  });
+
+    res.sendStatus(200); // Send a successful response
 });
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+app.listen(3000, () => console.log('Server listening on port 3000'));
